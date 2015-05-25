93da78c33d02bff2f555626a2d13db1bcb54813a
m8rge <to.merge@gmail.com>
2015-05-25 22:27:06 +0500

    update

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
+#!/usr/bin/php
 <?php
 
 /** @var \Composer\Autoload\ClassLoader $autoLoader */
-$autoLoader = require_once __DIR__.'/vendor/autoload.php';
-$autoLoader->addPsr4('m8rge\\', __DIR__);
+use DI\Container;
+use m8rge\Action;
 
-$app = new Silly\Application('app', 'dev-master');
+const AUTH_FILENAME = 'dropbox-auth.json';
+const API_IDENTIFIER = 'Dup-finder/dev-master';
+
+$autoLoader = require_once __DIR__.'/vendor/autoload.php';
+$autoLoader->addPsr4('m8rge\\', __DIR__. '/src');
+
+$app = new Silly\Edition\PhpDi\Application();
+
+/** @var Container $container */
+$container = $app->getContainer();
+$container->set('app', $app);
+
+$app->command('action', Action::class)->descriptions('');
+
+$app->run();
---
+++ src/Action.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace m8rge;
+
+use Silly\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Action
+{
+    function __invoke(Application $app, InputInterface $input, OutputInterface $output)
+    {
+    }
+}
